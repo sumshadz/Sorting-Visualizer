bc2ac451508a42a24a8848781eaad3521dd8eb13
sumshadz <shadangisuman@gmail.com>
2023-07-16 13:49:58 +0530

    customizing array length and sorting speed

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -15,6 +15,8 @@ constructor(props){
 	super(props);
 	this.state = {
 		array: [],
+		arrayLength: 90,
+		sortingSpeed: 0,
 	};
 }
 
@@ -24,10 +26,23 @@ componentDidMount(){
 
 resetArray(){
 	const array = [];
-	for(let i=0;i<this.ArrayLength;i++){
+	for(let i=0;i<this.state.arrayLength;i++){
 		array.push(randomIntFromInterval(this.ArrayMinElement,this.ArrayMaxElement));
 	}
 	this.setState({array});
+}
+
+setArrayLength(e){
+	const arr = [];
+	for(let i=0;i<e.target.value;i++){
+		arr.push(randomIntFromInterval(this.ArrayMinElement,this.ArrayMaxElement));
+	}
+	this.setState({array: arr, arrayLength: e.target.value});
+}
+
+setSortingSpeed(e){
+	const newValue = 8 - parseInt(e.target.value);
+	this.setState({sortingSpeed: newValue > 1 ? newValue : 1});
 }
 
 merge_Sort() {
@@ -43,14 +58,14 @@ merge_Sort() {
 			setTimeout(() => {
 				barOneStyle.backgroundColor = color;
 				barTwoStyle.backgroundColor = color;
-			}, i*this.SortingSpeed);
+			}, i*this.state.sortingSpeed);
 		}
 		else{
 			setTimeout(()=>{
 				const [barOneIdx, newHeight] =  animations[i];
 				const barOneStyle = arrayBars[barOneIdx].style;
 				barOneStyle.height = `${newHeight}px`;
-			}, i*this.SortingSpeed);
+			}, i*this.state.sortingSpeed);
 		}
 	}
 }
@@ -68,14 +83,14 @@ quick_Sort() {
 			setTimeout(() => {
 				barOneStyle.backgroundColor = color;
 				barTwoStyle.backgroundColor = color;
-			}, i*this.SortingSpeed);
+			}, i*this.state.sortingSpeed);
 		}
 		else{
 			setTimeout(()=>{
 				const [barOneIdx, newHeight] =  animations[i];
 				const barOneStyle = arrayBars[barOneIdx].style;
 				barOneStyle.height = `${newHeight}px`;
-			}, i*this.SortingSpeed);
+			}, i*this.state.sortingSpeed);
 		}
 	}
 }
@@ -92,11 +107,19 @@ render(){
 		))}
 		</div>
 <div className="menu">
+	<div className="range">
+	<span>Array Length</span>
+	<input  className="arrayLength" type="range" max={90} min={10} value={this.state.arrayLength} onChange={(e)=>this.setArrayLength(e)}/>
+	</div>
+	<div className="range">
+	<span>Sorting Speed</span>
+	<input  className="arrayLength" type="range" max={8} min={1} value={8-this.state.sortingSpeed} onChange={(e)=>this.setSortingSpeed(e)}/>
+	</div>
 		<button onClick={()=>this.resetArray()} id="generateArray">Generate New Array</button>
 		<button onClick={()=>this.merge_Sort()}>Merge Sort</button>
-		{/* <button onClick={()=>this.bubbleSort()}>Bubble Sort</button> */}
+		<button onClick={()=>this.bubbleSort()}>Bubble Sort</button>
 		{/* <button onClick={()=>this.heapSort()}>Heap Sort</button> */}
-		<button onClick={()=>this.quick_Sort()}>Quick Sort</button>
+		{/* <button onClick={()=>this.quick_Sort()}>Quick Sort</button> */}
 		</div>
 		</div>
 	) 
